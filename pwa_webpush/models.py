af39ac8e541ec22963705f99f64416938826a610
Amos Vryhof <amos@vryhofresearch.com>
2019-05-08 14:53:41 -0400

    Make models return a __str__ method

--- pwa_webpush/models.py
+++ pwa_webpush/models.py
@@ -1,23 +1,37 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.core.exceptions import FieldError
 from django.conf import settings
 
 from django.db.models import DO_NOTHING
+from django.utils.encoding import python_2_unicode_compatible
 
 from .app_settings import PWA_APP_NAME
 
 
+@python_2_unicode_compatible
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
 
+        return 'Push Group: %s' % self.name
+
+
+@python_2_unicode_compatible
 class SubscriptionInfo(models.Model):
     browser = models.CharField(max_length=100)
     endpoint = models.URLField(max_length=255)
     auth = models.CharField(max_length=100)
     p256dh = models.CharField(max_length=100)
 
+    def __str__(self):
 
+        return 'Subscription Information for %s' % self.browser
+
+
+@python_2_unicode_compatible
 class PushInformation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='webpush_info', blank=True, null=True,
                              on_delete=models.CASCADE)
@@ -33,7 +47,12 @@ class PushInformation(models.Model):
         else:
             raise FieldError('At least user or group should be present')
 
+    def __str__(self):
 
+        return 'Push Subscription for: %s' % self.user
+
+
+@python_2_unicode_compatible
 class PushMessage(models.Model):
     active = models.BooleanField(default=True)
     send_on = models.DateTimeField(auto_now_add=True, null=True)
@@ -43,3 +62,12 @@ class PushMessage(models.Model):
     icon = models.URLField(blank=True, null=True)
     send_to = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=DO_NOTHING)
     sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.sent:
+            retn = '%s, sent on %s' % (self.title, self.send_on)
+
+        else:
+            retn = '%s, to be sent on %s' % (self.title, self.send_on)
+
+        return retn
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements = [
 
 setup(
     name='django-pwa-webpush',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Public License',
